b4fa4d03e362fef888def97832887311fe0bdb93
Schema <112175566+M-Schema@users.noreply.github.com>
2023-05-19 17:27:24 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,33 +1,26 @@
 import java.util.Random;
 
 /**
- * @version 2023-05-03
  * @author Schema <WF221.M.Schema@BK-Duisdorf.de>
  */
 
 public class Player {
-	private String name = "";
-	private boolean machine = false;
-	private String input = "";
-	private int amount = 0;
-	private boolean winner = false;
-	
-	public Player() {
-	}
-	
+	private String name;
+	private boolean machine;
+	private String input;
+	private int amount;
+	private boolean winner;
+
 	public Player(String name) {
 		this.name = name;
-	}
-	
-	public Player(String name, boolean machine) {
-		this.name = name;
-		this.machine = machine;
+		this.machine = false;
+		this.winner = false;
 	}
 
 	public void setName(String name) {this.name = name;}	
 	public String getName() {return this.name;}
 	
-	public void setMachine(boolean machine) {this.machine = machine;}	
+	private void setMachine(boolean machine) {this.machine = machine;}	
 	public boolean isMachine() {return this.machine;}
 
 	public void setInput(String input) {this.input = input;}	
@@ -42,22 +35,20 @@ public class Player {
 	
 	public void optionMachine(String option) {
 		if ( !option.equals("0") || option.equals("") ) {
-				this.setName("Deus ex Machina");
-				this.setMachine(true);		
+			this.setMachine(true);		
 		}else {
 			this.setMachine(false);
 		}
 	}
 	
 	public void machineKillerStrategy(int matches) {
-		if(this.machine && matches%4 != 0) {
+		if (this.machine && (matches%4 != 0) ) {
 			this.setAmount(matches%4);
-		}else if(matches == 12) {
+		}else if (matches == 12) {
 			this.setAmount(1);
 		}else {
 			Random random = new Random();
 			this.setAmount(random.nextInt(3)+1);
 		}
 	}
-
 }
